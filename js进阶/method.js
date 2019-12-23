7d0265293b605683ee87cccf603fcbef3da1bb36
a-xcm <517353374@qq.com>
2019-12-23 15:06:47 +0800

    method.js添加了方法

--- js进阶/method.js
+++ js进阶/method.js
@@ -1,1 +1,21 @@
 "use strict";
+/**
+ * 多个数字求和
+ */
+function sum(...num) {
+	let sum = 0;
+	num.forEach(item => {
+		sum += item;
+	})
+	return sum;
+}
+
+/**
+ * 数组去重
+ * @param {Object} arry1
+ * @param {Object} arry2
+ */
+function uniq(arry1, arry2) {
+	return Array.from(new Set([...arry1,...arry2]))
+}
+
